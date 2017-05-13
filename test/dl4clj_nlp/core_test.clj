92c9fa38d701b8dad612bc6e8d3a6222c3bdd66f
hswick <hswick@smu.edu>
2017-05-13 14:57:08 -0500

    Addes stopper and stemmer and tests for each

--- test/dl4clj_nlp/core_test.clj
+++ test/dl4clj_nlp/core_test.clj
@@ -12,38 +12,41 @@
 
 (def w2v (nlp/word-2-vec "neuromancer.txt"))
 
-;(nlp/fit! w2v)
+(nlp/fit! w2v)
 
-;(deftest word-2-vec-test
-;  (is (= 100 (count (nlp/words-nearest w2v "chiba" 100))))
-;  (is (= true (nlp/has-word? w2v "chiba")))
-;  (is (= true (not (nil? (nlp/get-word-vector w2v "chiba")))))
-;  (is (= java.lang.Double (class (nlp/similarity w2v "chiba" "city")))))
+(deftest word-2-vec-test
+  (is (= 100 (count (nlp/words-nearest w2v "chiba" 100))))
+  (is (= true (nlp/has-word? w2v "chiba")))
+  (is (= true (not (nil? (nlp/get-word-vector w2v "chiba")))))
+  (is (= java.lang.Double (class (nlp/similarity w2v "chiba" "city")))))
 
-;(nlp/write-word-vectors w2v "data/neuromancer.csv")
+(nlp/write-word-vectors w2v "data/neuromancer.csv")
 
-;(def w2v2 (nlp/read-word-vectors (clojure.java.io/file "data/neuromancer.csv")))
+(def w2v2 (nlp/read-word-vectors (clojure.java.io/file "data/neuromancer.csv")))
 
-;(deftest serialization-test
-;  (is (= true (.exists (clojure.java.io/file "data/neuromancer.csv"))))
-;  (is (= org.deeplearning4j.models.word2vec.Word2Vec (class w2v2)))
-;  (is (= 100 (count (nlp/words-nearest w2v "chiba" 100)))))
+(deftest serialization-test
+  (is (= true (.exists (clojure.java.io/file "data/neuromancer.csv"))))
+  (is (= org.deeplearning4j.models.word2vec.Word2Vec (class w2v2)))
+  (is (= 100 (count (nlp/words-nearest w2v "chiba" 100)))))
 
-;(deftest stop-words
-;  (is (= true (seq? (nlp/stop-words)))))
+(def stopwords (nlp/stop-words))
 
-;(def w2v3 (nlp/word-2-vec 
-;            (iter/default-iterator (nlp/absolute-path "data/neuromancer.txt"))
-;         (token/default-tokenizer-factory (token/common-stemmer-preprocessor))
-;         {:min-word-frequency 6
-;          :stopwords (nlp/stop-words)
-;         :window-size 10
-;         :layer-size 150)))
+(deftest stopwords-test
+  (is (< 0 (.size stopwords)))
+  (is (= true (not (nil? stopwords)))))
+
+(def w2v3 (nlp/word-2-vec 
+            (iter/default-iterator (nlp/absolute-path "neuromancer.txt"))
+            (token/default-tokenizer-factory (token/common-stemmer-preprocessor))
+            {:min-word-frequency 6
+             :stopwords (nlp/stop-words)
+             :window-size 10
+             :layer-size 150}))
               
-;;(nlp/fit! w2v3)
+(nlp/fit! w2v3)
 
-;(deftest word-2-vec3-test
-;  (is (= 100 (count (nlp/words-nearest w2v3 "chiba" 100))))
-;  (is (= true (nlp/has-word? w2v3 "chiba")))
-;  (is (= true (not (nil? (nlp/get-word-vector w2v3 "chiba")))))
-;  (is (= java.lang.Double (class (nlp/similarity w2v3 "chiba" "city")))))
+(deftest word-2-vec3-test
+  (is (= 100 (count (nlp/words-nearest w2v3 "chiba" 100))))
+  (is (= true (nlp/has-word? w2v3 "chiba")))
+  (is (= true (not (nil? (nlp/get-word-vector w2v3 "chiba")))))
+  (is (= java.lang.Double (class (nlp/similarity w2v3 "chiba" "city")))))
